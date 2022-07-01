c48e4821c5f94226a6a0147521a24f6aaedaf0c6
Lily319 <leftbehindgd2009@gmail.com>
2022-07-01 19:51:41 +0800

    加入專案檔案。

---
+++ prjAjaxLab/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace prjAjaxLab.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
